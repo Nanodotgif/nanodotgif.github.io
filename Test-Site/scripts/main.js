c0e9d50b7daf8babb9c499518a9f855fdff7d66a
Nanodotgif <117686517+Nanodotgif@users.noreply.github.com>
2022-11-07 16:25:31 -0500

    Add files via upload

---
+++ Test-Site/scripts/main.js
@@ -0,0 +1,36 @@
+
+const image = document.querySelector("img");
+
+image.onclick = ()=> {
+    const currentSrc = image.getAttribute("src");
+    if (currentSrc === "images/Cyber-Corn-Woman.jpg") {
+        image.setAttribute("src", "images/Caught-In-4K.jpg");
+    } else {
+        image.setAttribute("src", "images/Cyber-Corn-Woman.jpg");
+    }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("What should I call you?");
+    if (myName === "") {
+        setUserName();
+        return;
+    }
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `> hi, youre ${myName}`;
+  }
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `> welcome back, ${storedName}`;
+}
+
+myButton.onclick = ()=> {
+    setUserName();
+}
+
